a500120f8b65c829cfb91848cf4e177c4d50eb84
gudian1618 <1761844211@qq.com>
2020-11-03 22:11:29 +0800

    Initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,14 @@
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2020/11/3 9:54 下午
+ */
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        ConnectionFactory f = new ConnectionFactory();
+    }
+}
